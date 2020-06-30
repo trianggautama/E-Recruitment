5bdbbf1bbb4aa868e2861e8c8f853d6a8cca8830
muhammadasadbastomi <tomycules@gmail.com>
2020-06-30 18:52:26 +0800

    setup lowongan migration

---
+++ database/migrations/2020_06_27_095318_create_lowongans_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLowongansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lowongans', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 36);
+            $table->unsignedBigInteger('berita_id');
+            // $table->unsignedBigInteger('posisi_id');
+            $table->json('posisi');
+            $table->string('keterangan', 100);
+            $table->double('ipk_min')->nullable();
+            $table->string('pendidikan_terakhir', 20);
+            $table->date('tgl_mulai');
+            $table->date('tgl_selesai');
+            $table->tinyInteger('status');
+            $table->foreign('berita_id')->references('id')->on('beritas')->onDelete('cascade');
+            // $table->foreign('posisi_id')->references('id')->on('posisis')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lowongans');
+    }
+}
