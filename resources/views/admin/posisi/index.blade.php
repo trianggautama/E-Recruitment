eb1e9dbc59ece4b48a19499dfbb2527ae2b96180
muhammadasadbastomi <tomycules@gmail.com>
2020-06-30 18:53:34 +0800

    backend index posisi view

--- resources/views/admin/posisi/index.blade.php
+++ resources/views/admin/posisi/index.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.main')
 
 @section('content')
- <!-- Main Content -->
- <div class="hk-pg-wrapper">
-			<!-- Container -->
-            <div class="container mt-xl-50 mt-sm-30 mt-15">
-				<!-- Title -->
-				<div class="hk-pg-header align-items-top">
-					<div>
-						<h2 class="hk-pg-title font-weight-600 mb-10">Halaman Posisi</h2>
-					</div>
-					<div class="d-flex">
-						<button class="btn btn-sm btn-outline-light btn-wth-icon icon-wthot-bg mr-15 mb-15"><span class="icon-label"><i class="fa fa-print"></i> </span><span class="btn-text">Print </span></button>
-						<button class="btn btn-sm btn-danger btn-wth-icon icon-wthot-bg mb-15" id="tambah"><span class="icon-label"><i class="fa fa-plus" ></i> </span><span class="btn-text">Tambah Data </span></button>
-					</div>
-				</div>
-				<!-- /Title -->
-				<!-- Row -->
-                <div class="row">
-                    <div class="col-xl-12">
-						<div class="hk-row">
-							<div class="col-lg-12">
-								
-                            <section class="hk-sec-wrapper">
+<!-- Main Content -->
+<div class="hk-pg-wrapper">
+    <!-- Container -->
+    <div class="container mt-xl-50 mt-sm-30 mt-15">
+        <!-- Title -->
+        <div class="hk-pg-header align-items-top">
+            <div>
+                <h2 class="hk-pg-title font-weight-600 mb-10">Halaman Posisi</h2>
+            </div>
+            <div class="d-flex">
+                <button class="btn btn-sm btn-outline-light btn-wth-icon icon-wthot-bg mr-15 mb-15"><span
+                        class="icon-label"><i class="fa fa-print"></i> </span><span class="btn-text">Print
+                    </span></button>
+                <button class="btn btn-sm btn-danger btn-wth-icon icon-wthot-bg mb-15" id="tambah"><span
+                        class="icon-label"><i class="fa fa-plus"></i> </span><span class="btn-text">Tambah Data
+                    </span></button>
+            </div>
+        </div>
+        <!-- /Title -->
+        <!-- Row -->
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="hk-row">
+                    <div class="col-lg-12">
+
+                        <section class="hk-sec-wrapper">
                             <h5 class="hk-sec-title">Data Table</h5>
                             <br>
                             <div class="row">
@@ -38,21 +42,28 @@
                                                 </tr>
                                             </thead>
                                             <tbody>
+                                                @foreach($data as $d)
                                                 <tr>
-                                                    <td>1</td>
-                                                    <td>Teknisi Jaringan</td>
-                                                    <td>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit modi omnis necessitatibus voluptas maiores voluptatibus quo tempora! Eum</td>
-                                                    <td>						
-                                                        <a href="{{Route('posisiEdit','hdiahdiah')}}" class="btn btn-sm btn-outline-light  mb-1"><span class="icon-label"><i class="fa fa-edit"></i> </span><span class="btn-text"> </span></a>
-                                                        <button class="btn btn-sm btn-outline-light  mb-1"><span class="icon-label"><i class="fa fa-trash"></i> </span><span class="btn-text"> </span></button>
+                                                    <td>{{$loop->iteration}}</td>
+                                                    <td>{{$d->nama}}</td>
+                                                    <td>{{$d->keterangan}}</td>
+                                                    <td>
+                                                        <a href="{{Route('posisiEdit',['uuid' => $d->uuid])}}"
+                                                            class="btn btn-sm btn-outline-light  mb-1"><span
+                                                                class="icon-label"><i class="fa fa-edit"></i>
+                                                            </span><span class="btn-text"> </span></a>
+                                                        <button class="btn btn-sm btn-outline-light  mb-1"><span
+                                                                class="icon-label"><i class="fa fa-trash"></i>
+                                                            </span><span class="btn-text"> </span></button>
                                                     </td>
                                                 </tr>
+                                                @endforeach
                                             </tbody>
                                             <tfoot>
                                                 <tr>
                                                     <th>No</th>
                                                     <th>Nama</th>
-                                                    <th>Username</th>
+                                                    <th>Keterangan</th>
                                                     <th>Aksi</th>
                                                 </tr>
                                             </tfoot>
@@ -61,52 +72,54 @@
                                 </div>
                             </div>
                         </section>
-							</div>
-						</div>
-					</div>
-                </div>
-                <!-- /Row -->
-            </div>
-            <!-- /Container -->
-			
-        </div>
-        <!-- /Main Content -->
-
-    <!-- Modal forms-->
-        <div class="modal fade" id="exampleModalForms" tabindex="-1" role="dialog" aria-labelledby="exampleModalForms" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                     <div class="modal-header">
-                        <h5 class="modal-title" id="status">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
                     </div>
-                    <div class="modal-body">
-                        <form>
-                            <div class="form-group">
-                                <label for="exampleDropdownFormEmail1">Nama Jabatan</label>
-                                <input type="text" class="form-control" id="nama" placeholder="nama">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleDropdownFormEmail1">Keterangan</label>
-                                <textarea name="" id="" class="form-control"></textarea>
-                            </div>
-                            <div class="text-right">
-                                <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Tambah Data</button>
-                            </div>
-                    </form>
                 </div>
-             </div>
+            </div>
         </div>
+        <!-- /Row -->
+    </div>
+    <!-- /Container -->
+
+</div>
+<!-- /Main Content -->
+
+<!-- Modal forms-->
+<div class="modal fade" id="exampleModalForms" tabindex="-1" role="dialog" aria-labelledby="exampleModalForms"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="status">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{Route('posisiCreate')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="exampleDropdownFormEmail1">Nama Posisi</label>
+                        <input type="text" class="form-control" name="nama" id="nama" placeholder="nama">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleDropdownFormEmail1">Keterangan</label>
+                        <textarea name="keterangan" id="" class="form-control"></textarea>
+                    </div>
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Tambah Data</button>
+                    </div>
+                </form>
+            </div>
         </div>
+    </div>
+</div>
 
 @endsection
 @section('scripts')
-    <script>
-       $("#tambah").click(function(){
+<script>
+    $("#tambah").click(function(){
             $('#status').text('Tambah Data');
             $('#exampleModalForms').modal('show');
         });
-    </script>
+</script>
 @endsection
