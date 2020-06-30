10587959f7c5c07b460751315a448574cc7ec965
muhammadasadbastomi <tomycules@gmail.com>
2020-06-30 18:51:54 +0800

    setup user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('uuid', 36);
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('username')->unique();
+            $table->string('password')->nullable();
+            $table->string('role')->default(1);
+            $table->string('foto')->default('default.jpg');
+            $table->string('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
