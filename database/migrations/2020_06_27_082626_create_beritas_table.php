1905819940b0494582dbfcd5236913d8d86b4445
muhammadasadbastomi <tomycules@gmail.com>
2020-06-30 18:52:18 +0800

    setup berita migration

---
+++ database/migrations/2020_06_27_082626_create_beritas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeritasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beritas', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 36);
+            $table->unsignedBigInteger('user_id');
+            $table->string('judul', 100);
+            $table->text('isi');
+            $table->string('foto', 100)->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beritas');
+    }
+}
