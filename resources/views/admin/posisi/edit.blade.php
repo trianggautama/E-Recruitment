13177238b9e84b658acff7660bdbebf74574f560
muhammadasadbastomi <tomycules@gmail.com>
2020-06-30 18:53:14 +0800

    backend posisi edit view

--- resources/views/admin/posisi/edit.blade.php
+++ resources/views/admin/posisi/edit.blade.php
@@ -1,65 +1,71 @@
 @extends('layouts.main')
 
 @section('content')
- <!-- Main Content -->
- <div class="hk-pg-wrapper">
-			<!-- Container -->
-            <div class="container mt-xl-50 mt-sm-30 mt-15">
-				<!-- Title -->
-				<div class="hk-pg-header align-items-top">
-					<div>
-						<h2 class="hk-pg-title font-weight-600 mb-10">Halaman Edit Posisi</h2>
-					</div>
-				</div>
-				<!-- /Title -->
-				<!-- Row -->
-                <div class="row">
-                    <div class="col-xl-12">
-						<div class="hk-row">
-							<div class="col-lg-12">
-								
-                            <section class="hk-sec-wrapper">
+<!-- Main Content -->
+<div class="hk-pg-wrapper">
+    <!-- Container -->
+    <div class="container mt-xl-50 mt-sm-30 mt-15">
+        <!-- Title -->
+        <div class="hk-pg-header align-items-top">
+            <div>
+                <h2 class="hk-pg-title font-weight-600 mb-10">Halaman Edit Posisi</h2>
+            </div>
+        </div>
+        <!-- /Title -->
+        <!-- Row -->
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="hk-row">
+                    <div class="col-lg-12">
+
+                        <section class="hk-sec-wrapper">
                             <h5 class="hk-sec-title">Edit Posisi</h5>
                             <br>
                             <div class="row">
                                 <div class="col-sm">
                                     <div class="table-wrap">
-                                    <form>
-                                    <div class="form-group">
-                                        <label for="exampleDropdownFormEmail1">Nama Jabatan</label>
-                                        <input type="text" class="form-control" id="nama" placeholder="nama">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleDropdownFormEmail1">Keterangan</label>
-                                        <textarea name="" id="" class="form-control"></textarea>
-                                    </div>
-                                            <div class="text-right">
-                                                <a href="{{Route('posisiIndex')}}" class="btn btn-secondary"><i class="fa fa-arrow-left"></i> Kembali</a>
-                                                <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> Ubah Data</button>
+                                        <form method="POST">
+                                            @method('PUT')
+                                            @csrf
+                                            <div class="form-group">
+                                                <label for="exampleDropdownFormEmail1">Nama Posisi</label>
+                                                <input type="text" class="form-control" name="nama" id="nama"
+                                                    value="{{$data->nama}}" placeholder="nama">
                                             </div>
-                                    </form>
+                                            <div class="form-group">
+                                                <label for="exampleDropdownFormEmail1">Keterangan</label>
+                                                <textarea name="keterangan" id=""
+                                                    class="form-control">{{$data->keterangan}}</textarea>
+                                            </div>
+                                            <div class="text-right">
+                                                <a href="{{Route('posisiIndex')}}" class="btn btn-secondary"><i
+                                                        class="fa fa-arrow-left"></i> Kembali</a>
+                                                <button type="submit" class="btn btn-danger"><i class="fa fa-save"></i>
+                                                    Ubah Data</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                         </section>
-							</div>
-						</div>
-					</div>
+                    </div>
                 </div>
-                <!-- /Row -->
             </div>
-            <!-- /Container -->
-			
         </div>
-        <!-- /Main Content -->
+        <!-- /Row -->
+    </div>
+    <!-- /Container -->
+
+</div>
+<!-- /Main Content -->
 
 
 @endsection
 @section('scripts')
-    <script>
-       $("#tambah").click(function(){
+<script>
+    $("#tambah").click(function(){
             $('#status').text('Tambah Data');
             $('#exampleModalForms').modal('show');
         });
-    </script>
+</script>
 @endsection
