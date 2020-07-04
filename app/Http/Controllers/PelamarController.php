437828ce29b2a2c9189198159d35a071a2638ee6
muhammadasadbastomi <tomycules@gmail.com>
2020-07-04 13:42:58 +0800

    add lampiran store

--- app/Http/Controllers/PelamarController.php
+++ app/Http/Controllers/PelamarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Lampiran;
 use App\Lowongan;
 use App\Peserta;
 use App\User;
@@ -37,6 +38,43 @@ class PelamarController extends Controller
 
         $data = $user->peserta()->create($req->all());
 
+        $lampiran = new Lampiran;
+        $lampiran->user_id = $user->id;
+        if ($req->doc_ijazah != null) {
+            $img = $req->file('doc_ijazah');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $user->id;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('lampiran/ijazah', $foto);
+            $lampiran->doc_ijazah = $foto;
+        }
+        if ($req->foto_ktp != null) {
+            $img = $req->file('foto_ktp');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $user->id;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('lampiran/ktp', $foto);
+            $lampiran->foto_ktp = $foto;
+        }
+        if ($req->doc_transkip != null) {
+            $img = $req->file('doc_transkip');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $user->id;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('lampiran/transkip', $foto);
+            $lampiran->doc_transkip = $foto;
+        }
+        if ($req->doc_npwp != null) {
+            $img = $req->file('doc_npwp');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $user->id;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('lampiran/npwp', $foto);
+            $lampiran->doc_npwp = $foto;
+        }
+
+        $lampiran->save();
+
         return redirect()->route('depan')->withSuccess('Berhasil input lamaran, silahkan login menggunakan NIK sebagai username & password');
     }
 
@@ -70,6 +108,9 @@ class PelamarController extends Controller
     public function lulusSeleksiShow($uuid)
     {
         $lowongan = Lowongan::where('uuid', $uuid)->first();
-        return view('admin.pelamar.seleksiBerkasShow', compact('lowongan'));
+        $data = Peserta::whereHas('user', function ($query) {
+            $query->where('status', 1);
+        })->with('user')->get();
+        return view('admin.pelamar.seleksiBerkasShow', compact('lowongan', 'data'));
     }
 }
