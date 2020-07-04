16b6f021d6340ef87b2a422bb031e78790034a02
muhammadasadbastomi <tomycules@gmail.com>
2020-07-04 12:59:33 +0800

    add swal css

--- resources/views/lowonganInput.blade.php
+++ resources/views/lowonganInput.blade.php
@@ -507,6 +507,7 @@
 	============================================= -->
 
 
+	@include('sweetalert::alert')
 	<!-- js -->
 	<script type="text/javascript" src="{{asset('depan/assets/js/jquery-2.1.4.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('depan/assets/js/bootstrap.min.js')}}"></script>
