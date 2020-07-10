fae01a78c6e5467601212d25118471cf5b7958d2
muhammadasadbastomi <tomycules@gmail.com>
2020-07-10 15:51:40 +0800

    setup crud uji kompetensi and input soal peserta

--- app/Http/Controllers/ujiKompetensiController.php
+++ app/Http/Controllers/ujiKompetensiController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Jawaban_peserta;
 use App\Lowongan;
 use App\Soal;
 use App\Uji_kompetensi;
+use App\Uji_kompetensi_peserta;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ujiKompetensiController extends Controller
 {
@@ -13,31 +18,97 @@ class ujiKompetensiController extends Controller
 
         $data = Uji_kompetensi::latest()->get();
         $lowongan = Lowongan::latest()->get();
-        return view('admin.ujiKompetensi.index', compact('data', 'lowongan'));
+        $now = Carbon::now()->format('Ymd');
+        return view('admin.ujiKompetensi.index', compact('data', 'lowongan', 'now'));
+    }
+
+    public function store(Request $req)
+    {
+        $data = Uji_kompetensi::create($req->all());
+
+        return back()->withSuccess('Data berhasil disimpan');
     }
 
     public function edit($uuid)
     {
         $data = Uji_kompetensi::where('uuid', $uuid)->first();
-        return view('admin.ujiKompetensi.edit', compact('data'));
+        $lowongan = Lowongan::latest()->get();
+        return view('admin.ujiKompetensi.edit', compact('data', 'lowongan'));
     }
 
-    public function show()
+    public function update(Request $req, $uuid)
     {
+        $data = Uji_kompetensi::where('uuid', $uuid)->first();
+        $data->fill($req->all())->save();
 
-        return view('admin.ujiKompetensi.show');
+        return redirect()->route('ujiKompetensiIndex')->withSuccess('Data berhasil diubah');
+    }
+
+    public function show($uuid)
+    {
+        $data = Uji_kompetensi::where('uuid', $uuid)->first();
+        $peserta = Uji_kompetensi_peserta::where('uji_kompetensi_id', $data->id)->get();
+
+        return view('admin.ujiKompetensi.show', compact('data', 'peserta'));
     }
 
     public function input()
     {
 
-        $soals = Soal::oldest()->get();
-        return view('user.ujiKompetensi.input', compact('soals'));
+        $peserta_id = Auth::user()->peserta->id;
+        $data = Uji_kompetensi::orderBy('id', 'desc')->first();
+        $tesPeserta = Uji_kompetensi_peserta::where('peserta_id', $peserta_id)->where('uji_kompetensi_id', $data->id)->first();
+
+        if (!$tesPeserta) {
+            $tesPeserta = new Uji_kompetensi_peserta;
+            $tesPeserta->peserta_id = $peserta_id;
+            $tesPeserta->uji_kompetensi_id = $data->id;
+            $tesPeserta->save();
+
+            $soalData = Soal::all();
+            $soals = $soalData->shuffle()->take(20);
+            return view('user.ujiKompetensi.input', compact('soals'));
+        } else {
+
+            return back()->withWarning('Anda sudah melakukan uji kompetensi');
+        }
+    }
+
+    public function inputStore(Request $req)
+    {
+        $peserta_id = Auth::user()->peserta->id;
+        $data = Uji_kompetensi::orderBy('id', 'desc')->first();
+        $tesPeserta = Uji_kompetensi_peserta::where('peserta_id', $peserta_id)->where('uji_kompetensi_id', $data->id)->first();
+        $soal = collect($req->soal_id)->filter();
+        $pilihan = collect($req->pilihan)->filter();
+        for ($i = 0; $i < count($soal); $i++) {
+            $jawabanSoal = Soal::findOrFail($soal[$i]);
+            if ($jawabanSoal->kunci == $pilihan[$i]) {
+                $bs = 1;
+            } else {
+                $bs = 0;
+            }
+            $jawaban = new Jawaban_peserta();
+            $jawaban->soal_id = $soal[$i];
+            $jawaban->jawaban = $pilihan[$i];
+            $jawaban->bs = $bs;
+            $jawaban->uji_kompetensi_peserta_id = $tesPeserta->id;
+            $jawaban->save();
+        }
+        $peserta = Jawaban_peserta::where('uji_kompetensi_peserta_id', $tesPeserta->id)->where('bs', 1)->count();
+        $tesPeserta->nilai = $peserta * 5;
+        $tesPeserta->update();
+        return redirect()->route('pelamarIndex')->withSuccess('Berhasil melakukan ujian! Nilai anda adalah : <b>' . $tesPeserta->nilai . '</b>');
+
     }
 
     public function hasil()
     {
-
+        $peserta_id = Auth::user()->peserta->id;
+        $tesPeserta = Uji_kompetensi_peserta::where('peserta_id', $peserta_id)->first();
+        if (!$tesPeserta) {
+            return back()->withWarning('Anda belu');
+        }
         return view('user.ujiKompetensi.hasil');
     }
 }
