3ebc96ed5a270b807cac426072a3c1a549c546d6
muhammadasadbastomi <tomycules@gmail.com>
2020-07-10 15:50:41 +0800

    create model uji kesehatan

---
+++ app/Uji_kesehatan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+class Uji_kesehatan extends Model
+{
+    use Uuid;
+
+    protected $guarded = [
+
+    ];
+
+    public function lowongan()
+    {
+        return $this->belongsTo(Lowongan::class);
+    }
+
+}
