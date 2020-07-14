d411ba818c21adf9fa8d409676259d6718544880
muhammadasadbastomi <tomycules@gmail.com>
2020-07-14 20:05:55 +0800

    setup migration uji wawancara peserta tables

---
+++ database/migrations/2020_07_13_213103_create_uji_wawancara_pesertas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUjiWawancaraPesertasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uji_wawancara_pesertas', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 36);
+            $table->unsignedBigInteger('uji_wawancara_id');
+            $table->unsignedBigInteger('peserta_id');
+            $table->text('catatan');
+            $table->tinyInteger('status');
+            $table->foreign('uji_wawancara_id')->references('id')->on('uji_wawancaras')->onDelete('cascade');
+            $table->foreign('peserta_id')->references('id')->on('pesertas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uji_wawancara_pesertas');
+    }
+}
