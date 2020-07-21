35eea1d890d144abd54727d2cfcd2d0fbd697b2d
muhammadasadbastomi <tomycules@gmail.com>
2020-07-21 22:52:22 +0800

    fix lowongan

--- resources/views/admin/pelamar/index.blade.php
+++ resources/views/admin/pelamar/index.blade.php
@@ -8,7 +8,7 @@
         <!-- Title -->
         <div class="hk-pg-header align-items-top">
             <div>
-                <h2 class="hk-pg-title font-weight-600 mb-10">Halaman Pelamar Pada Lwongan {{$lowongan->berita->judul}}
+                <h2 class="hk-pg-title font-weight-600 mb-10">Halaman Pelamar Pada Lowongan {{$lowongan->berita->judul}}
                 </h2>
             </div>
             <div class="d-flex text-right">
