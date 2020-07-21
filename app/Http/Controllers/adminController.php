bd3e9da7ce09e139d12c513a078d3fc8844d79ee
muhammadasadbastomi <tomycules@gmail.com>
2020-07-21 15:06:41 +0800

    make var now for validation date

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Berita;
 use App\Lowongan;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class adminController extends Controller
@@ -18,8 +19,9 @@ class adminController extends Controller
     }
 
     public function lowonganDetail($uuid){
+        $now = Carbon::now()->format('Y-m-d');
         $berita = Berita::where('uuid',$uuid)->first();
-        return view('lowonganDetail',compact('berita'));
+        return view('lowonganDetail',compact('berita','now'));
     }
 
     public function lowonganInput($uuid){
