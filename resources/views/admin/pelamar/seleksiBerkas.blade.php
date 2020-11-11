efc36d312c3b59243f5647d915ba4a5fca8bb504
tri angga utama <trianggategarutama@gmail.com>
2020-11-11 09:20:34 +0800

    hapus analisis lowongan di seleksi berkas

--- resources/views/admin/pelamar/seleksiBerkas.blade.php
+++ resources/views/admin/pelamar/seleksiBerkas.blade.php
@@ -11,9 +11,6 @@
                 <h2 class="hk-pg-title font-weight-600 mb-10">Halaman Informasi Pelamar Lulus Pemberkasan</h2>
             </div>
             <div class="d-flex">
-                <a href="" class="btn btn-sm btn-danger btn-wth-icon icon-wthot-bg mb-15"><span
-                        class="icon-label"><i class="fa fa-print"></i> </span><span class="btn-text">Analisis lowongan
-                    </span></a>
             </div>
         </div>
         <!-- /Title -->
